0689c6d1bce0e2e44ae3ecdb75380fbfcc503ba7
mailtest52837-hue <mailtest52837@gmail.com>
2025-08-12 04:51:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ from flask_cors import CORS
 from db import Base, engine
 from routes import routes
 
+import os
+
 app = Flask(__name__)
+port = int(os.environ.get("PORT", 5000))
 CORS(app)
 
 Base.metadata.create_all(bind=engine)
 app.register_blueprint(routes)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=port)
