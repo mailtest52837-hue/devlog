a602be0db0e9fc38279d921f590be204a905ecf2
mailtest52837-hue <mailtest52837@gmail.com>
2025-08-12 04:45:53 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "postgresql://artemnikolaev:qwertylike1663@localhost:5432/devlog"
+DATABASE_URL = "postgresql://devlogdb_2umy_user:zC8l3ifKGOxOunJBbhaNAvYKhlQnTZPz@dpg-d2d9on3e5dus73fogkr0-a.oregon-postgres.render.com/devlogdb_2umy"
 
 engine = create_engine(DATABASE_URL)
 
